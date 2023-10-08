afe085ca60a83e5ce4e7716356e1c88959c531ea
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-10-08 08:35:26 +0600

    add short description and show details button

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,8 +1,8 @@
 
 const Service = ({service}) => {
-    const {name, image, price, description } = service
+    const {name, image, price, ShortDescription } = service
     return (
-        <div className="border hover:border-yellow-500 bg-blend-overlay w-72">
+        <div className="border hover:border-yellow-500 flex flex-col bg-blend-overlay w-[310px]">
             <img className="w-full h-56" src={image} alt="" />
             <div className="flex mt-4">
             <div className="bg-custom-yellow h-13 w-2  "></div>
@@ -12,6 +12,8 @@ const Service = ({service}) => {
             <p className="text-left pl-2 font-medium text-custom-yellow">Tickets from: {price}</p>
             </div>
             </div>
+            <p className="text-left pl-3 flex-grow">{ShortDescription}</p>
+            <button className="btn my-3 hover:text-white  hover:bg-gradient-to-br from-[#FF3E00] to-[#FFBE30]">Click to see details</button>
         </div>
     );
 };
